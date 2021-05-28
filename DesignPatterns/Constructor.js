0e86fb8a7bcf6869a47805b43edb2645ddc0ed84
Sukhpreet <sukhseera@gmail.com>
2021-05-28 22:13:50 +0100

    constructor pattern

--- DesignPatterns/Constructor.js
+++ DesignPatterns/Constructor.js
@@ -1,1 +1,36 @@
+/* This is a class-based creational design pattern. 
 
+ Constructors are special functions that can be used to instantiate new objects 
+ with methods and properties defined by that function. */
+
+
+// traditional Function-based syntax
+function Hero(name, specialAbility){
+    // setting property values
+    this.name = name;
+    this.specialAbility = specialAbility;
+
+    // declaring a method on the object
+    this.getDetails = function(){
+        return this.name + 'can' + this.specialAbility;
+    };
+}
+
+// ES6 Class syntax
+class Hero {
+    constructor(name, specialAbility){
+        // setting property values
+        this._name = name;
+        this._specialAbility = specialAbility;
+
+        // declaring a method on the object
+        this.getDetails = function(){
+        return `$(this._name) can $(this._specialAbility)`
+        };
+    }
+}
+
+// creating new instances of Hero
+const IronMan = new Hero('Iron Man', 'fly');
+
+console.log(IronMan.getDetails()); // Iron Man can fly
--- DesignPatterns/README.md
+++ DesignPatterns/README.md
@@ -1,1 +1,12 @@
+A design pattern is a term used in software engineering for a general, reusable solution to a commonly occurring problem in software design.
 
+Patterns also make your code more reusable and readable while speeding up the development process vastly.
+
+Creational Design Patterns : A creational design pattern basically solves a problem by controlling the creation process of an object.
+Constructor Pattern, Factory Pattern, Prototype Pattern, and Singleton Pattern.
+
+Structural Design Patterns : These patterns are concerned with class and object composition.
+Adapter Pattern, Composite Pattern, Decorator Pattern, Façade Pattern, Flyweight Pattern, and Proxy Pattern.
+
+Behavioral Design Patterns : These patterns are concerned with improving communication between dissimilar objects.
+Chain of Responsibility Pattern, Command Pattern, Iterator Pattern, Mediator Pattern, Observer Pattern, State Pattern, Strategy Pattern, and Template Pattern.
