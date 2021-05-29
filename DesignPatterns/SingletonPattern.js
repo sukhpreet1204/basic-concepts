5aaccf637c8df44397b634cfa0d3c0bf1a9029c4
Sukhpreet Kaur <84335966+sukhpreet1204@users.noreply.github.com>
2021-05-29 23:18:45 +0100

    Singleton Pattern

---
+++ DesignPatterns/SingletonPattern.js
@@ -0,0 +1,37 @@
+/* Singleton is a special creational design pattern in which only one instance of a class can exist.
+
+It works like this — if no instance of the singleton class exists 
+then a new instance is created and returned, 
+but if an instance already exists, then the reference to the existing instance is returned.
+
+A perfect real-life example would be that of mongoose (the famous Node.js ODM library for MongoDB). 
+It utilizes the singleton pattern.
+*/
+
+class Database {
+    constructor(data){
+        if(Database.exists){
+            return Database.instance;
+        }
+
+        this._data = data;
+        Database.instance = this;
+        Database.exists = true;
+        return this;
+    }
+
+    getData() {
+        return this._data;
+      }
+
+    setData(data) {
+        this._data = data;
+    }
+}
+
+const mongo = new Database('mongo');
+console.log(mongo.getData()); // mongo
+
+const mysql = new Database('mysql');
+console.log(mysql.getData()); // mongo
+
