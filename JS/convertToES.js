847b15930948e057591a5e40c753c31fabe13366
Sukhpreet Kaur <84335966+sukhpreet1204@users.noreply.github.com>
2021-05-31 19:32:02 +0100

    Convert to ES

---
+++ JS/convertToES.js
@@ -0,0 +1,27 @@
+/*
+Convert class based inheritance code given below to ES5 code.
+class Parent(name){
+    constructor(name) {
+        this.name = name
+    }
+    getName(){ return this.name }
+}
+class Children extends Parent {
+    constructor(props) {
+        super(props)
+    }
+}
+ */
+
+function Parent(name){
+    this.name = name;
+}
+
+Parent.prototype.getName = function(){
+    return this.name;
+}
+
+function Children(name){
+    Parent.call(this.name);
+}
+Children.prototype = new Parent();
