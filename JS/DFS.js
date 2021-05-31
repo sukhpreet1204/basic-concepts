1c99341059b621bccc53533389a1e231f4aed07a
Sukhpreet Kaur <84335966+sukhpreet1204@users.noreply.github.com>
2021-05-31 23:42:20 +0100

    DFS

---
+++ JS/DFS.js
@@ -0,0 +1,21 @@
+/*
+Create a function that, given a DOM Element on the page, will visit the element itself 
+and all of its descendents (not just its immediate children). 
+
+For each element visited, the function should pass that element to a provided callback function.
+
+The arguments to the function should be:
+a DOM element
+a callback function (that takes a DOM element as its argument)
+
+Visiting all elements in a tree (DOM) is a classic Depth-First-Search algorithm application. 
+Here’s an example solution:
+*/
+
+function Traverse(p_element, p_callback){
+    p_callback(p_element);
+    var list = p_element.children;
+    for(var i=0; i<list.length; i++){
+        Traverse(list[i], p_callback);      //recursive call
+    }
+}
