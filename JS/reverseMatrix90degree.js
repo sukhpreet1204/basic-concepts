b5ac7440980db24c934ecdc3835d64b4daff3820
Sukhpreet Kaur <84335966+sukhpreet1204@users.noreply.github.com>
2021-06-14 19:58:35 +0100

    reverse matrix program

---
+++ JS/reverseMatrix90degree.js
@@ -0,0 +1,15 @@
+function reversematrix(matrix){
+    for(let i=0; i<matrix.length; i++){
+        for(let j=i+1; j<matrix[i].length;j++){
+            let temp=matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    for(let i=0; i<matrix.length; i++){
+        matrix[i].reverse();
+    }
+}
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]];
