580c10df8631d8a966245d75e4ab2649bd949f60
Sukhpreet Kaur <84335966+sukhpreet1204@users.noreply.github.com>
2021-06-29 10:04:17 +0100

    NaN

---
+++ JS/Nan.js
@@ -0,0 +1,24 @@
+parseInt("foo")
+//prints NaN
+
+Number(undefined);
+//prints NaN
+
+Number("foo");
+//prints NaN
+
+Math.sqrt(-1);
+//prints NaN
+
+0 * Infinity;
+//prints NaN
+
+undefined + undefined;
+//prints NaN
+
+"foo"/3;
+//prints NaN
+
+0/0;
+//prints NaN
+
